d0c61c5b1e94f2f7c28e50539f150d37fb193279
dinadehbi <147596316+dinadehbi@users.noreply.github.com>
2024-10-09 11:10:12 +0100

    Delete tuto1-3.kt

--- tuto1-3.kt
+++
@@ -1,9 +0,0 @@
-fun birthdayGreeting(name: String, age: Int): String {
-    val nameGreeting = "Happy Birthday, $name!"
-    val ageGreeting = "You are now $age years old!"
-    return "$nameGreeting\n$ageGreeting"
-}
-fun main() {
-    println(birthdayGreeting("Rover", 5))
-    println(birthdayGreeting("Rex", 2))
-}
